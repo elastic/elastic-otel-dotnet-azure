a732de5ff018fb49696e1508c1bfa4f6465e4a92
Martijn Laarman <Mpdreamz@gmail.com>
2024-09-05 13:44:57 +0200

    Ensure dotnet-api builds when using az acr build commandline

--- infra/azAcrBuild.sh
+++ infra/azAcrBuild.sh
@@ -4,12 +4,23 @@ set -e
 # Make sure az commands use the same subscription
 az account set -s $AZURE_SUBSCRIPTION_ID
 
-printf "Building and uploading otel-collector image to $ACR_NAME. Please wait..."
+printf "Building and uploading elastic-otel-collector image to $ACR_NAME. Please wait..."
 az acr build --resource-group $GROUP_NAME \
   --registry $ACR_NAME \
-  --image my-otel-collector:latest \
-  --file images/otel-collector/Dockerfile \
-  --platform linux \
+  --image elastic-otel-collector-azure:latest \
+  --file images/elastic-otel-collector/Dockerfile \
+  --platform linux/amd64 \
   --output none \
-  images/otel-collector
+  images/elastic-otel-collector
+printf "Success!\n"
+
+
+printf "Building and uploading dotnet-api image to $ACR_NAME. Please wait..."
+az acr build --resource-group $GROUP_NAME \
+  --registry $ACR_NAME \
+  --image dotnet-api:latest \
+  --file images/dotnet-api/Dockerfile \
+  --platform linux/amd64 \
+  --output none \
+  images/dotnet-api
 printf "Success!\n"
